99ee217f4f0b9b1c6177897bc9af0226cec4fc69
unknown <camelia_luciana@yahoo.com>
2020-03-29 16:26:44 +0300

    rounded boxes and signup from

--- frontend/iss/src/model.js
+++
@@ -1,6 +0,0 @@
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
