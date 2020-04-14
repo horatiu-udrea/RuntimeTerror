dd7e7c874d55343c686b73d97a3b43a56835ba03
Horațiu Udrea <udrea.horatiu@gmail.com>
2020-04-14 12:01:33 +0300

    Begun work on ORM

---
+++ backend/src/orm/Tables.kt
@@ -0,0 +1,20 @@
+package ro.runtimeterror.cms.orm
+
+import org.jetbrains.exposed.sql.Table
+
+object Users : Table()
+{
+    val id = varchar("id", 10) // Column<String>
+    val name = varchar("name", length = 50) // Column<String>
+    val cityId = (integer("city_id") references Cities.id).nullable() // Column<Int?>
+
+    override val primaryKey = PrimaryKey(id, name = "PK_User_ID") // name is optional here
+}
+
+object Cities : Table()
+{
+    val id = integer("id").autoIncrement() // Column<Int>
+    val name = varchar("name", 50) // Column<String>
+
+    override val primaryKey = PrimaryKey(id, name = "PK_Cities_ID")
+}
