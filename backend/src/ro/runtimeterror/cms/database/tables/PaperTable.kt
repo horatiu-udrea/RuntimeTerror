c6c731a78bd8f1a8400fc6d08c789d0f521abeb2
Razvan <razvantoma707@yahoo.com>
2020-05-04 19:19:18 +0300

    Updated Tables (Database): - FK_UserID added to PaperTable.kt - SectionTable.kt updated (typing mistake on sessionChair)

--- backend/src/ro/runtimeterror/cms/database/tables/PaperTable.kt
+++ backend/src/ro/runtimeterror/cms/database/tables/PaperTable.kt
@@ -4,6 +4,7 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 
 object PaperTable : IntIdTable("Papers", "PK_PaperID")
 {
+    var userid = reference("FK_UserID", UserTable.id)
     val field = varchar("Field", 100)
     val documentPath = varchar("DocumentPath", 100)
     val conflicting = bool("Conflicting")
