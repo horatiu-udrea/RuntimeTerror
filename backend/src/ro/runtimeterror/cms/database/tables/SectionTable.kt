a4d0d2108d1791f4a68ed70f9bd7e7f158f9b401
Jamil <jamil.zaitouny@gmail.com>
2020-05-10 17:27:59 +0300

    Removed unnecessary import

--- backend/src/ro/runtimeterror/cms/database/tables/SectionTable.kt
+++ backend/src/ro/runtimeterror/cms/database/tables/SectionTable.kt
@@ -2,11 +2,10 @@ package ro.runtimeterror.cms.database.tables
 
 import org.jetbrains.exposed.dao.id.IntIdTable
 import org.jetbrains.exposed.sql.jodatime.datetime
-import ro.runtimeterror.cms.database.tables.RoomTable
 
 object SectionTable : IntIdTable("Sections", "PK_SectionID")
 {
-    val sesscionChair = reference("FK_SessionChair", UserTable.id).nullable()
+    val sessionChair = reference("FK_SessionChair", UserTable.id).nullable()
     val name = varchar("Name", 100)
     val description = varchar("Description", 500)
     val startTime = datetime("StartTime")
