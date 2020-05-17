ff0987604926a2289279f89edfe8a547ae666692
Jamil <jamil.zaitouny@gmail.com>
2020-05-17 14:30:09 +0300

    Made a small change to PaperSubmissionTable.kt
    Made it so that It's possible to insert values into the columns through DSL operations

--- backend/src/ro/runtimeterror/cms/database/tables/PaperSubmissionTable.kt
+++ backend/src/ro/runtimeterror/cms/database/tables/PaperSubmissionTable.kt
@@ -5,8 +5,13 @@ import org.jetbrains.exposed.sql.Table
 object PaperSubmissionTable : Table("PaperSubmissions")
 {
 
-    val paperID =  PaperSubmissionTable.reference("FK_PaperID", PaperTable.id)
-    val userID = PaperSubmissionTable.reference("FK_UserID", UserTable.id)
+
+    val paperID = integer("paper")
+            .references(PaperTable.id)
+//                    PaperSubmissionTable.reference("FK_PaperID", PaperTable.id)
+    val userID = integer("user")
+        .references(UserTable.id)
+//        PaperSubmissionTable.reference("FK_UserID", UserTable.id)
 
     override val primaryKey = PrimaryKey(
         paperID,
