251bc8fce3b7cc6d2d49a96443c4d6701f41c28f
Horațiu Udrea <udrea.horatiu@gmail.com>
2020-05-19 14:32:04 +0300

    Fix some conflicts regarding paper review and decision

--- backend/src/ro/runtimeterror/cms/controller/PaperReviewController.kt
+++ backend/src/ro/runtimeterror/cms/controller/PaperReviewController.kt
@@ -1,25 +1,22 @@
 package ro.runtimeterror.cms.controller
 
-import ro.runtimeterror.cms.model.PaperReview
 import org.jetbrains.exposed.sql.insert
-import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.transactions.transaction
-import ro.runtimeterror.cms.database.DatabaseSettings
-import ro.runtimeterror.cms.database.daos.PaperDAO
 import ro.runtimeterror.cms.database.tables.ReviewTable
+import ro.runtimeterror.cms.model.PaperReview
 import ro.runtimeterror.cms.model.Qualifier
-import ro.runtimeterror.cms.model.Review
 import ro.runtimeterror.cms.model.validators.PaperValidator
 import ro.runtimeterror.cms.model.validators.UserValidator
 
 class PaperReviewController
 {
     /**
-     * Get all the review made by the user or assigned to him
+     * Get all the reviews made by the user or assigned to him and also the other reviews for the same paper
+     * If the author is a PC member, he is not allowed to see the other reviews
      */
     fun getReviews(userId: Int): List<PaperReview>
     {
-        val reviews: MutableList<Review> = ArrayList()
+        /*val reviews: MutableList<Review> = ArrayList()
         transaction (DatabaseSettings.connection){
             ReviewTable
                     .select { ReviewTable.userID eq userId }
@@ -30,7 +27,8 @@ class PaperReviewController
                         )
                     }
         }
-        return reviews
+        return reviews*/
+        TODO()
     }
 
     /**
