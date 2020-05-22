9b9913aa9338b4491ebb54594fa6878cbfa17775
Razvan <razvantoma707@yahoo.com>
2020-05-22 18:01:30 +0300

    Update currentPhase in the conference table for all the phases

--- database/SecondPhase2.sql
+++ database/SecondPhase2.sql
@@ -34,7 +34,7 @@ ALTER TABLE users AUTO_INCREMENT = 1;
 ALTER TABLE papers AUTO_INCREMENT = 1;
 
 /* The conference */
-INSERT INTO cms.conference (name, currentphase, startdate, enddate, submissiondeadline, proposaldeadline, biddingdeadline, submitpaperearly) VALUES ('Oracle Java One', 0, '2020-09-01 14:00:00', '2020-09-03 20:00:00', '2020-08-01 22:00:00', '2020-08-15 22:00:00', '2020-08-20 22:00:00', 1);
+INSERT INTO cms.conference (name, currentphase, startdate, enddate, submissiondeadline, proposaldeadline, biddingdeadline, submitpaperearly) VALUES ('Oracle Java One', 2, '2020-09-01 14:00:00', '2020-09-03 20:00:00', '2020-08-01 22:00:00', '2020-08-15 22:00:00', '2020-08-20 22:00:00', 1);
 
 /*
 Steering committee members:
