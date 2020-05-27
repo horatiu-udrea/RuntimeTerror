f835cc901e6832f55de09ec4b8dc0fd427d87803
Horațiu Udrea <udrea.horatiu@gmail.com>
2020-05-27 20:37:17 +0300

    Beautify file upload code

--- backend/src/ro/runtimeterror/cms/networking/FileUpload.kt
+++ backend/src/ro/runtimeterror/cms/networking/FileUpload.kt
@@ -7,14 +7,8 @@ import io.ktor.http.content.forEachPart
 import io.ktor.http.content.streamProvider
 import io.ktor.request.receiveMultipart
 import io.ktor.util.pipeline.PipelineContext
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import kotlinx.coroutines.yield
 import ro.runtimeterror.cms.exceptions.ProgramException
 import java.io.File
-import java.io.InputStream
-import java.io.OutputStream
 
 const val directory: String = "files"
 
@@ -23,50 +17,19 @@ suspend fun PipelineContext<Unit, ApplicationCall>.uploadFile(userId: Int): Stri
     val multipart = call.receiveMultipart()
     var savedFile: String? = null
     multipart.forEachPart { part ->
-        when (part)
+        if (part is PartData.FileItem)
         {
-            is PartData.FileItem ->
-            {
-                val ext = File(part.originalFileName!!).extension
-                val filename = "upload-${userId.hashCode()}-${System.currentTimeMillis()}.$ext"
-                part.streamProvider().use { input ->
-                    File("$directory/$filename").outputStream().buffered().use { output ->
-                        input.copyTo(output)
-                    }
+            val originalFileName = part.originalFileName ?: throw ProgramException("File not uploaded!")
+            val ext = File(originalFileName).extension
+            val filename = "upload-${userId.hashCode()}-${System.currentTimeMillis()}.$ext"
+            part.streamProvider().use { input ->
+                File("$directory/$filename").outputStream().buffered().use { output ->
+                    input.copyTo(output)
                 }
-                savedFile = filename
             }
+            savedFile = filename
         }
-
         part.dispose()
     }
     return savedFile ?: throw ProgramException("File not uploaded!")
-}
-
-
-suspend fun InputStream.copyToSuspend(
-    out: OutputStream,
-    bufferSize: Int = DEFAULT_BUFFER_SIZE,
-    yieldSize: Int = 4 * 1024 * 1024,
-    dispatcher: CoroutineDispatcher = Dispatchers.IO
-): Long
-{
-    return withContext(dispatcher) {
-        val buffer = ByteArray(bufferSize)
-        var bytesCopied = 0L
-        var bytesAfterYield = 0L
-        while (true)
-        {
-            val bytes = read(buffer).takeIf { it >= 0 } ?: break
-            out.write(buffer, 0, bytes)
-            if (bytesAfterYield >= yieldSize)
-            {
-                yield()
-                bytesAfterYield %= yieldSize
-            }
-            bytesCopied += bytes
-            bytesAfterYield += bytes
-        }
-        return@withContext bytesCopied
-    }
 }
