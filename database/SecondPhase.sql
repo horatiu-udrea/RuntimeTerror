0641ddb9899ee274126fb5f43dc78f8a2f2e5c31
Razvan <razvantoma707@yahoo.com>
2020-05-27 16:37:12 +0300

    Updated All Phases and Added Third Phase

--- database/SecondPhase.sql
+++ database/SecondPhase.sql
@@ -1,5 +1,5 @@
 /*
- This file will be used to simulate the first phase of execution of our program:
+ This file will be used to simulate the second phase of execution of our program:
  -- In the second phase, PC members are required to bid the proposals. Once the bidding
  -- process closed, the conference chair or co-chairs assign to each reviewer the papers
  -- required to be evaluated. By default any reviewer does not receive for evaluation a paper
@@ -28,11 +28,13 @@ DELETE FROM conference;
 DELETE FROM reviews;
 DELETE FROM bidpapers;
 DELETE FROM papersubmissions;
+DELETE FROM usersectionchoices;
+DELETE FROM sections;
 DELETE FROM papers;
 DELETE FROM users;
 ALTER TABLE users AUTO_INCREMENT = 1;
 ALTER TABLE papers AUTO_INCREMENT = 1;
-
+ALTER TABLE sections AUTO_INCREMENT = 1;
 /* The conference */
 INSERT INTO cms.conference (name, currentphase, startdate, enddate, submissiondeadline, proposaldeadline, biddingdeadline, submitpaperearly) VALUES ('Oracle Java One', 2, '2020-09-01 14:00:00', '2020-09-03 20:00:00', '2020-08-01 22:00:00', '2020-08-15 22:00:00', '2020-08-20 22:00:00', 1);
 
@@ -87,14 +89,14 @@ Abstracts and Papers:
 -- 4 of them will also have the document path for the full paper
  */
 INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('soft pentru medicina', 'medicinaAbstract', 'medicina', 'medicina, software', 'medicina Topic', '', 1);
-INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('AiPaper', 'ITAbstract', 'ai', 'ai, artificial intelligence', 'ai Topic', './FolderFiles/aiPaper.pdf', 1);
+INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('AiPaper', 'ITAbstract', 'ai', 'ai, artificial intelligence', 'ai Topic', '../files/aiPaper.pdf', 1);
 INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('soft pentru masini', 'masiniAbstract', 'masini', 'masini, software', 'masini Topic', '', 1);
-INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('MPPPaper', 'MPPAbstract', 'mpp', 'mpp, sdi', 'mpp Topic', './FolderFiles/mppPaper.pdf', 1);
+INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('MPPPaper', 'MPPAbstract', 'mpp', 'mpp, sdi', 'mpp Topic', '../files/mppPaper.pdf', 1);
 /*
 INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('Fuzzy systems', 'fuzzyAbstract', 'fuzzy systems', 'fuzzy systems, fuzzy logic, software', 'fuzzy Topic', '', 1);
-INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('ISSPaper', 'ISSAbstract', 'iss', 'iss, software engineering', 'iss Topic', './FolderFiles/issPaper.pdf', 1);
+INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('ISSPaper', 'ISSAbstract', 'iss', 'iss, software engineering', 'iss Topic', '../files/issPaper.pdf', 1);
 INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('Car parking soft', 'carAbstract', 'software, cars, car', 'medicina, software', 'car Topic', '', 1);
-INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('WEBPaper', 'WEBAbstract', 'web', 'web, web programming', 'web Topic', './FolderFiles/webPaper.pdf', 1);
+INSERT INTO cms.papers (name, abstract, field, keywords, topics, documentpath, status) VALUES ('WEBPaper', 'WEBAbstract', 'web', 'web, web programming', 'web Topic', '../files/webPaper.pdf', 1);
  */
 INSERT INTO cms.papersubmissions (fk_paperid, fk_userid) VALUES (1, 18) ON DUPLICATE KEY UPDATE fk_userid = 18;
 INSERT INTO cms.papersubmissions (fk_paperid, fk_userid) VALUES (2, 19) ON DUPLICATE KEY UPDATE fk_userid = 19;
