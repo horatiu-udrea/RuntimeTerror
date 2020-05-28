55641be26a5cf64b0ee8c4c2d480e0b10d6f31ad
Razvan <razvantoma707@yahoo.com>
2020-05-28 13:45:50 +0300

    Removed uniqueIndex from the username field in the UserTable

--- backend/src/ro/runtimeterror/cms/database/tables/UserTable.kt
+++ backend/src/ro/runtimeterror/cms/database/tables/UserTable.kt
@@ -5,7 +5,7 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 object UserTable : IntIdTable("Users", "PK_UserID")
 {
     val name = varchar("name", 50)
-    val username = varchar("username", 50).uniqueIndex()
+    val username = varchar("username", 50)
     val password = varchar("password", 50)
     val affiliation = varchar("affiliation", 50)
     val email = varchar("email", 50).uniqueIndex()
