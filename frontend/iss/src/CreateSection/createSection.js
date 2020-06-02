12fbd659ef06de29f84750e80f8ceba617cc84a2
CameliaZalum <cameliazalum@gmail.com>
2020-06-02 13:25:03 +0300

    slideshow ready

--- frontend/iss/src/CreateSection/createSection.js
+++ frontend/iss/src/CreateSection/createSection.js
@@ -66,7 +66,6 @@ $(document).ready(function () {
                             + "<br>")
                     })
                 });
-                console.log(inSelect);
                 fillPapersAndDetails(inSelect);
             }
         }
